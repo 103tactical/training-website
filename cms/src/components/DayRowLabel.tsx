99904d9e08f67f09409437165872205ca8f5f53a
103tactical <103tactical@users.noreply.github.com>
2026-04-14 04:05:22 -0400

    Fix DayRowLabel: handle rowNumber possibly undefined

--- cms/src/components/DayRowLabel.tsx
+++ cms/src/components/DayRowLabel.tsx
@@ -4,5 +4,5 @@ import { useRowLabel } from '@payloadcms/ui'
 
 export default function DayRowLabel() {
   const { rowNumber } = useRowLabel()
-  return <span>Day {rowNumber + 1}</span>
+  return <span>Day {(rowNumber ?? 0) + 1}</span>
 }
