31984c7cdc42f83131474797bffa25fcfcbd3c58
103tactical <103tactical@users.noreply.github.com>
2026-04-13 23:51:24 -0400

    Increase admin logo height to 160px

--- cms/src/components/AdminLogo.tsx
+++ cms/src/components/AdminLogo.tsx
@@ -9,7 +9,7 @@ export default function AdminLogo() {
     <img
       src={theme === 'dark' ? '/img/103_logo_admin_white.png' : '/img/103_logo_admin_black.png'}
       alt="103 Tactical"
-      style={{ height: '52px', width: 'auto' }}
+      style={{ height: '160px', width: 'auto' }}
     />
   )
 }
